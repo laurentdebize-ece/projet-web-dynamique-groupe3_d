c2ac1a7ca8af0766da8c740dc1e11d69c3f60b03
Mona Lias <mona.lias@edu.ece.fr>
2023-05-04 19:36:19 +0200

    creation des pages d'accueil pour les 3 roles

---
+++ admin.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Accueil</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <section id="section1">
+            <div class="section1_partition1">
+                <div class="partition1_head">
+                    <div class="img_container">
+                        <div class="img"> </div>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="promo.php">Promotions & Classes</a></li>
+                            <li><a href="prof.php">Professeurs</a></li>
+                            <li><a href="matieres.php">Matières</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1 class=" welcom_mess">Omnes Skills</h1>
+            </div>
+            <div class="section1_partition2">
+                <div class="carte-container">
+                    <div class="carte carte1">
+
+                        <div class="carte_img carte_img1">
+
+                        <a href="promo.php"><h3>Promotions & Classes</h3></a>
+
+                        </div>
+
+                    </div>
+                    <div class="carte carte2">
+
+                        <div class="carte_img carte_img2">
+                        <a href="prof.php"><h3>Professeurs</h3></a>
+                        </div>
+
+                    </div>
+                    <div class="carte carte3">
+
+                        <div class="carte_img carte_img3">
+
+                        <a href="matieres.php"><h3>Matières</h3></a>
+
+                        </div>
+                    </div>
+                    <div class="carte carte4">
+
+                        <div class="carte_img carte_img4">
+                                <h3>Mon espace</h3>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="section1_partition3">
+                <h3>Evaluations à venir</h3>    
+            </div>
+
+
+        </section>
+    </header>
+</body>
+
+</html>
---
+++ etudiant.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Accueil</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <section id="section1">
+            <div class="section1_partition1">
+                <div class="partition1_head">
+                    <div class="img_container">
+                        <div class="img"> </div>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="matieres.php">Mes matières</a></li>
+                            <li><a href=".php">Mes évaluations</a></li>
+                            <li><a href=".php">Evaluations à venir</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1 class=" welcom_mess">Omnes Skills</h1>
+            </div>
+            <div class="section1_partition2">
+                <div class="carte-container">
+                    <div class="carte carte1">
+
+                        <div class="carte_img carte_img1">
+
+                        <a href="matieres.php"><h3>Mes matières</h3></a>
+
+                        </div>
+
+                    </div>
+                    <div class="carte carte2">
+
+                        <div class="carte_img carte_img2">
+                        <a href=".php"><h3>Mes évaluations</h3></a>
+                        </div>
+
+                    </div>
+                    <div class="carte carte3">
+
+                        <div class="carte_img carte_img3">
+
+                        <a href=".php"><h3>Évaluations à venir</h3></a>
+
+                        </div>
+                    </div>
+                    <div class="carte carte4">
+
+                        <div class="carte_img carte_img4">
+                                <h3>Mon espace</h3>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="section1_partition3">
+                <h3>Le planning</h3>    
+            </div>
+
+
+        </section>
+    </header>
+</body>
+
+</html>
