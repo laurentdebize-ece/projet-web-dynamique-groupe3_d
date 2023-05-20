86c8dee80c8ad30f43ea6ccad587b492ee721c26
Vadim LE MOULLEC <vadim.lemoullec@edu.ece.fr>
2023-05-20 16:35:02 +0200

    nav prof terminé sans les option de modif

--- navigationPromoProf.php
+++ navigationPromoProf.php
@@ -44,10 +44,12 @@
                 <p>
 
                     <?php
+                    $encore=0;
+                    $encore2=0;
                     $ID= $_SESSION['id'];
                     $response = $bdd->query("SELECT * FROM promotion AS p, professeurs AS pr, enseignerclassmatprof AS cmp, classe AS c WHERE pr.IdProf = '$ID' AND pr.IdProf =cmp.IdProf AND cmp.IdClasse=c.IdClasse AND p.ID=c.IdPromotion ORDER BY anneeDePromo");
                     while ($donnees = $response->fetch()) {
-                        $response2 = $bdd->query("SELECT * FROM professeurs AS pr, enseignerclassmatprof AS cmp, classe AS c WHERE pr.IdProf = '$ID' AND pr.IdProf =cmp.IdProf AND cmp.IdClasse=c.IdClasse ORDER BY classeNum");
+                        $response2 = $bdd->query("SELECT * FROM promotion AS p, professeurs AS pr, enseignerclassmatprof AS cmp, classe AS c WHERE pr.IdProf = '$ID' AND pr.IdProf =cmp.IdProf AND cmp.IdClasse=c.IdClasse AND p.ID=c.IdPromotion ORDER BY classeNum");
                         $MAX = $bdd->query('SELECT MAX(anneeDePromo) AS "Max" FROM promotion');
                         $MaxP = $MAX->fetch();
                         $MIN = $bdd->query('SELECT MIN(anneeDePromo) AS "Min" FROM promotion');
@@ -56,15 +58,18 @@
                         $MaxC = $MAXC->fetch();
                         $MINC = $bdd->query('SELECT MIN(classeNum) AS "MinC" FROM classe');
                         $MinC = $MINC->fetch();
-                        echo "<p class= promo id=p-" . $donnees["anneeDePromo"] . ">Promo " . $donnees['anneeDePromo'] . "</p>";
-                        while ($donnees2 = $response2->fetch()) {
-                            if ($donnees["anneeDePromo"] == $donnees2["anneeDePromo"]) {
-                                echo "<p class=c-" . $donnees["anneeDePromo"] . ">classe " . $donnees2["classeNum"] . "</p>";
-                            }
+                        if ($donnees["anneeDePromo"]!=$encore){
+                            echo "<p class= promo id=p-" . $donnees["anneeDePromo"] . ">Promo " . $donnees['anneeDePromo'] . "</p>";
+                            $encore=$donnees["anneeDePromo"];
+                            while ($donnees2 = $response2->fetch()) {
+                                if ($donnees["anneeDePromo"] == $donnees2["anneeDePromo"]) {
+                                    echo "<p class=c-" . $donnees["anneeDePromo"] . ">classe " . $donnees2["classeNum"] . "</p>";
+                                }
+                            }   
                         }
+                        
                     }
                     ?>
-                    <!-- id=c".$donnees2["classeNum"]."p".$donnees["anneeDePromo"]. -->
                 </p>
             </div>
             <div class="col-sm-8" id=listeClasses>
@@ -100,10 +105,7 @@
         <h2 class=titreNavClasse><span id='titreNavClasse'></span></h2>    
         <div class="row" id=ClasseNav>
             <div class="col-sm-4" id=etuMat>
-            <?php       
-
-                
-                $response = $bdd->query('SELECT * FROM etudiant AS e, classe AS c, promotion AS p WHERE c.IdClasse=e.IdClasse AND c.IdPromotion=p.ID ORDER BY nomEtu');
+            <?php                
                 $MAXE = $bdd->query('SELECT MAX(IdEtudiant) AS "Max" FROM etudiant');
                 $MaxE = $MAXE->fetch();
                 $MINE = $bdd->query('SELECT MIN(IdEtudiant) AS "Min" FROM etudiant');
@@ -112,82 +114,44 @@
                 $MaxM = $MAXM->fetch();
                 $MINM = $bdd->query('SELECT MIN(idMat) AS "Min" FROM matieres');
                 $MinM = $MINM->fetch();
-                    while ($donnees = $response->fetch()) {;
-                        $response2 = $bdd->query('SELECT * FROM etumat AS em, etudiant AS e, matieres AS m WHERE e.IdEtudiant=em.IdEtu AND em.IdMat=m.idMat ORDER BY nomMat');
-                        
-                        echo "<p class=etu-" . $donnees["anneeDePromo"]."-".$donnees["classeNum"]." id=e-" . $donnees["IdEtudiant"].">". $donnees["prenomEtu"]." ".$donnees["nomEtu"]."</p>";
-                        while ($donnees2 = $response2->fetch()){
-                            if ($donnees["IdEtudiant"] == $donnees2["IdEtudiant"]) {
-                                echo "<p class=m-".$donnees["IdEtudiant"]." id=m-".$donnees2["idMat"]."-".$donnees["IdEtudiant"].">" . $donnees2["nomMat"] . "</p>";
-                            }
-                        }
-                    }
+                $response = $bdd->query('SELECT * FROM etudiant AS e, classe AS c, promotion AS p WHERE c.IdClasse=e.IdClasse AND c.IdPromotion=p.ID ORDER BY nomEtu');
+                while ($donnees = $response->fetch()) {                    
+                    echo "<p class=etu-" . $donnees["anneeDePromo"]."-".$donnees["classeNum"]." id=e-" . $donnees["IdEtudiant"].">". $donnees["prenomEtu"]." ".$donnees["nomEtu"]."</p>";
+                }
                 ?>
             </div>
             <div class="col-sm-8" id=matiereListe>
-                <?php
-                    $nbcase= 0;
-                    $response = $bdd->query('SELECT * FROM etudiant AS e, classe AS c, promotion AS p WHERE c.IdClasse=e.IdClasse AND c.IdPromotion=p.ID ORDER BY nomEtu');
-                        while ($donnees = $response->fetch()) {
-                            $response2 = $bdd->query('SELECT * FROM etumat AS em, etudiant AS e, matieres AS m WHERE e.IdEtudiant=em.IdEtu AND em.IdMat=m.idMat ORDER BY nomMat');
-                            echo "<div class='container'><div class='row'>";
-                            while ($donnees2 = $response2->fetch()) {
-                                if ($donnees["IdEtudiant"] == $donnees2["IdEtudiant"]) {
-                                    echo "<div class='col-sm-4'><p class=mlist-" . $donnees["IdEtudiant"] . " id=mlist-". $donnees["IdEtudiant"]."-".$donnees2["idMat"].">" . $donnees2["nomMat"] . "</p></div>";
-                                    $nbcase= ($nbcase+1)%4;
-                                    if ($nbcase==0){
-                                        echo "</div><div class='row'>";
-                                    }
-                                }
-                            }
-                            if($nbcase!=4){
-                                for($i=0;$i<4-$nbcase;$i++){
-                                    echo "<div class='col-sm-4'></div>"; 
-                                }
-                            }
-                            echo "</div></div>";
-                        }
-                    ?>
-            </div>
-        </div>
-
-<!-- navigation des compétence en fonction de la matière séléctionner -->
-
-        <div class="row" id=MatiereNav>
-        <div class="col-sm-4" id=typeMat>
-            <?php  
-                $response = $bdd->query('SELECT * FROM matieres ORDER BY idMat');
-                while ($donnees = $response->fetch()) {
-                    echo "<h2 id=mNom-" . $donnees["idMat"] . ">" . $donnees['nomMat'] . "</h2>";
-                }
-                echo "<p id=CompClick>Compétences</p>";
-                echo "<p id=TransClick>Compétences Tranverses";      
-                ?>
-            </div>
-            <div class="col-sm-8">
             <div class='container'><div class='row' id=comp>
-            <div class='col-sm-4'>Nom de la Compétence</div>
-            <div class='col-sm-4'>évalué le :</div>
-            <div class='col-sm-4'>Niveau :</div>
+            <div class='col-sm-3'>Nom de la Compétence</div>
+            <div class='col-sm-3'>Matière</div>
+            <div class='col-sm-3'>évalué le :</div>
+            <div class='col-sm-3'>Niveau :</div>
             </div></div>
             <?php
-                $response = $bdd->query('SELECT * FROM etudiant AS e, eval AS ev, evalcomp AS ec, competences AS c, niveau AS n WHERE e.IdEtudiant=ev.IdEtu AND ev.IdEval=ec.IdEval AND ec.IdComp=c.idCompetence  AND n.idNiv=c.IdNiv ORDER BY nomEtu');
-                    while ($donnees = $response->fetch()) {
-                        $response2 = $bdd->query('SELECT * FROM etumat AS em, etudiant AS e, matieres AS m, competences AS c, matcomp AS mc WHERE e.IdEtudiant=em.IdEtu AND em.IdMat=m.idMat AND m.idMat=mc.IdMat AND mc.IdCompetence=c.idCompetence ORDER BY nomCompetence');
-                        while ($donnees2 = $response2->fetch()) {
-                            if ($donnees["idCompetence"] == $donnees2["idCompetence"] && $donnees2["IdEtudiant"]==$donnees["IdEtudiant"]) {
-                                echo "<div class='container'><div class='row'>";
-                                echo "<div class='col-sm-4'><p class=comp-" . $donnees2["IdEtudiant"] . "-" . $donnees2["idMat"] . "-" . $donnees["trans"] . ">" . $donnees["nomCompetence"] . "</div></p>"; 
-                                echo "<div class='col-sm-4'><p class=comp-" . $donnees2["IdEtudiant"] . "-" . $donnees2["idMat"] . "-" . $donnees["trans"] . ">" . $donnees["date"] . "</div></p>"; 
-                                echo "<div class='col-sm-4'><p class=comp-" . $donnees2["IdEtudiant"] . "-" . $donnees2["idMat"] . "-" . $donnees["trans"] . ">" . $donnees["niv"] . "</div></p>"; 
-                                echo "</div></div>";
+                $encore3=0;
+                $MATIERES =  $bdd->query("SELECT * FROM professeurs AS pr, enseignerclassmatprof AS cmp WHERE pr.IdProf = '$ID' AND pr.IdProf =cmp.IdProf");
+                while($Matieres=$MATIERES->fetch()){
+                    if($encore3!=$Matieres['IdMat']){
+                        $response = $bdd->query('SELECT * FROM etudiant AS e, eval AS ev, evalcomp AS ec, competences AS c, niveau AS n WHERE e.IdEtudiant=ev.IdEtu AND ev.IdEval=ec.IdEval AND ec.IdComp=c.idCompetence  AND n.idNiv=c.IdNiv ORDER BY nomEtu');
+                        while ($donnees = $response->fetch()) {
+                            $response2 = $bdd->query('SELECT * FROM etumat AS em, etudiant AS e, matieres AS m, competences AS c, matcomp AS mc WHERE e.IdEtudiant=em.IdEtu AND em.IdMat=m.idMat AND m.idMat=mc.IdMat AND mc.IdCompetence=c.idCompetence ORDER BY nomCompetence');
+                            while ($donnees2 = $response2->fetch()) {
+                                if ($donnees["idCompetence"] == $donnees2["idCompetence"] && $donnees2["IdEtudiant"]==$donnees["IdEtudiant"] && $donnees2["idMat"]==$Matieres['IdMat']) {
+                                    echo "<div class='container'><div class='row'>";
+                                    echo "<div class='col-sm-3'><p class=comp-" . $donnees2["IdEtudiant"] . "-" . $donnees["trans"] . ">" . $donnees["nomCompetence"] . "</div></p>"; 
+                                    echo "<div class='col-sm-3'><p class=comp-" . $donnees2["IdEtudiant"] . "-" . $donnees["trans"] . ">" . $donnees2["nomMat"] . "</div></p>";
+                                    echo "<div class='col-sm-3'><p class=comp-" . $donnees2["IdEtudiant"] . "-" . $donnees["trans"] . ">" . $donnees["date"] . "</div></p>"; 
+                                    echo "<div class='col-sm-3'><p class=comp-" . $donnees2["IdEtudiant"] . "-" . $donnees["trans"] . ">" . $donnees["niv"] . "</div></p>"; 
+                                    echo "</div></div>";
+                                }
                             }
                         }
+                        $encore3=$Matieres['IdMat'];
                     }
+                }
             ?>
             </div>
         </div>
-    </div>
 
     <!-- bouton de retour -->
     <p id=RETOUR>RETOUR</p>
@@ -215,18 +179,11 @@
                     $('.etu-'+promo+'-'+classe).hide();
                     $('.m-'+etudiant).hide('slow');
                     $('.mlist-'+etudiant).hide();
-                    navigation = 0;
-                    break;
-                case 2:
-                    $('.titreNavClasse').show();
-                    $('#ClassNav').show();
-                    $('#etuMat').show();
-                    $('#matiereListe').show();
                     $('#MatiereNav').hide(); 
                     $('#mNom-'+matiere).hide();
-                    $('.comp-'+etudiant+'-'+matiere+'-0').hide();
-                    $('.comp-'+etudiant+'-'+matiere+'-1').hide();
-                    navigation=1;
+                    $('.comp-'+etudiant+'-0').hide();
+                    $('#comp').hide();
+                    navigation = 0;
                     break;
             }
         });
@@ -276,28 +233,13 @@
     for ($p=$MinE["Min"] ; $p<=$MaxE["Max"] ; $p++){
         echo "<script>$(document).ready(function() {";
         echo "$('#e-".$p."').click(function() {";
-        echo "$('.m-".$p."').slideToggle('slow');";
-        echo "$('.mlist-".$p."').toggle();";
+        echo 'if(etudiant!='.$p.'){$(".comp-"+etudiant+"-0").hide();}';
+        echo "etudiant = ".$p.";";
+        echo "$('.comp-'+etudiant+'-0').toggle();";
+        echo "$('#comp').show();";
         echo "});});";
         echo "</script>";
         }  
-    for($t=$MinE["Min"] ; $t<=$MaxE["Max"] ; $t++){
-        for($y=$MinM["Min"] ; $y<=$MaxM["Max"] ; $y++){
-            echo "<script>$(document).ready(function() {";
-            echo "$('#mlist-".$t."-".$y."').click(function() {";
-            echo "$('.titreNavClasse').hide();"; 
-            echo "$('#ClassNav').hide();";
-            echo "$('#etuMat').hide();";
-            echo "$('#matiereListe').hide();";
-            echo "$('#MatiereNav').show();";  
-            echo "matiere = ".$y.";"; 
-            echo "etudiant = ".$t.";";
-            echo "$('#mNom-".$y."').show();"; 
-            echo "navigation=2;";   
-            echo "});});";
-            echo "</script>";
-        }
-    }
 
 ?>
 
