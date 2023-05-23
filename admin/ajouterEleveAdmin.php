4825d94ebacdcbe8a72d2391483aab1d48962a9b
Margaux Leroy <mrgx.lr6@gmail.com>
2023-05-23 23:44:40 +0200

    omggg la virguleeeee

--- admin/ajouterEleveAdmin.php
+++ admin/ajouterEleveAdmin.php
@@ -13,8 +13,6 @@
         $id_prenomEtu= $_POST['id_prenomEtu'];
         $id_mailEtu= $_POST['id_mailEtu'];
         $id_mdpEtu= $_POST['id_mdpEtu'];
-
-        
         try {
             $bdd = new PDO(
                 'mysql:host=localhost;dbname=omnes_skills;
@@ -36,7 +34,8 @@
                 die('Erreur : ' . $e->getMessage());
             }
         }
-        $request = 'INSERT INTO etudiant (nomEtu, prenomEtu, mdp, IdClasse, mailEtu) VALUES (:nomEtu, :prenomEtu, :mdp, :IdClasse, :mailEtu)';
+        $request = 'INSERT INTO etudiant (nomEtu, prenomEtu, mdp, IdClasse, mailEtu) 
+        VALUES (:nomEtu, :prenomEtu, :mdp, :IdClasse, :mailEtu)';
         $req = $bdd->prepare($request);
         $req->bindValue(':nomEtu', $id_nomEtu );
         $req->bindValue(':prenomEtu', $id_prenomEtu);
